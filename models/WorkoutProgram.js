80bd51629b5907fc6c14fbf4c98116382baaacc4
Laurence <larfoley@yahoo.com>
2020-07-25 16:23:07 +0100

    update

--- models/WorkoutProgram.js
+++ models/WorkoutProgram.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 const workoutProgramSchema = require('./shared-schema/workout-program');
 
 workoutProgramSchema.add({
-  isActive: { type: Boolean, default: false }
+  isActive: { type: Boolean, default: false },
+  startedOn: { type: Date },
+  completedOn: { type: Date },
 })
 
 module.exports = mongoose.model('WorkoutProgram', workoutProgramSchema);
